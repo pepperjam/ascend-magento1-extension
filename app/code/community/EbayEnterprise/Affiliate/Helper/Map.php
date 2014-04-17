156b56759e7732752c957026cdcec54909b0c63f
Scott VanBrug <svanbrug@ebay.com>
2014-04-17 00:40:59 -0400

    Generic Map Helper
    Add a helper for generic feed mapping callbacks.
    Fix one of the configured callbacks to use the proper helper.

--- app/code/community/EbayEnterprise/Affiliate/Helper/Map.php
+++ app/code/community/EbayEnterprise/Affiliate/Helper/Map.php
@@ -2,4 +2,54 @@
 
 class EbayEnterprise_Affiliate_Helper_Map
 {
+	/**
+	 * Get the program id using the store passed in params. Pass thru to the
+	 * config helper using the store included in the $params array as the store
+	 * context to get the config value from.
+	 * @param  array $params
+	 * @return string
+	 */
+	public function getProgramId($params)
+	{
+		return Mage::helper('eems_affiliate/config')->getProgramId($params['store']);
+	}
+	/**
+	 * Get data from the "item" object in the params at the "key" in the params
+	 * array. Method is highly dependent upon the $params array meeting some base
+	 * requirements: $params["item"] is a Varien_Object subclass and
+	 * $params["key"] is set.
+	 * @param  array $params
+	 * @return mixed
+	 */
+	public function getDataValue($params)
+	{
+		if (!$params['item'] instanceof Varien_Object) {
+			throw new Mage_Core_Exception(
+				sprintf(
+					'Item of type %s not compatible with %s',
+					get_class($params['item']), __METHOD__
+				)
+			);
+		}
+		if (!isset($params['key'])) {
+			throw new Mage_Core_Exception(
+				'The data "key" must be provided in the configured params for this callback.'
+			);
+		}
+		return $params['item']->getDataUsingMethod($params['key']);
+	}
+	/**
+	 * Simply return the "value" included in the params.
+	 * @param  array $params
+	 * @return string
+	 */
+	public function passStatic($params)
+	{
+		if (!isset($params['value'])) {
+			throw new Mage_Core_Exception(sprintf(
+				'No value provided to return from %s', __METHOD__
+			));
+		}
+		return $params['value'];
+	}
 }
