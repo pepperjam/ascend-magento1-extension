97dbbfc547c4cf28bbc5537d6c385c65e4d9afc0
Reginald Gabriel <rgabriel@ebay.com>
2014-06-11 14:30:42 -0400

    US12942 Adding Copyright and License
    Created LICENSE.md and FILE_LICENSE.txt file. Use content
    in FILE_LICENSE.txt file to append license comments on .php script
    files, .xml files and .phtml files.

--- app/code/community/EbayEnterprise/Affiliate/Test/Helper/Map/ProductTest.php
+++ app/code/community/EbayEnterprise/Affiliate/Test/Helper/Map/ProductTest.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Copyright (c) 2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the eBay Enterprise
+ * Magento Extensions End User License Agreement
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf
+ *
+ * @copyright   Copyright (c) 2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf  eBay Enterprise Magento Extensions End User License Agreement
+ *
+ */
+
 class EbayEnterprise_Affiliate_Test_Helper_Map_ProductTest
 	extends EcomDev_PHPUnit_Test_Case
 {
